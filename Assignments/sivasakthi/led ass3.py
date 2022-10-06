59465d05daa6859270f13cc6fbbecd18b3a2e048
sivasakthi2002 <114426076+sivasakthi2002@users.noreply.github.com>
2022-10-06 21:11:17 +0530

    Add files via upload

---
+++ Assignments/sivasakthi/led ass3.py
@@ -0,0 +1,12 @@
+import Rpi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.Board)
+
+GPIO.setup(3,GPIO.Out)
+
+while True:
+    GPIO.output(3,True)
+    time.sleep(1)
+    GPIO.output(3,False)
+    time.sleep(1)
